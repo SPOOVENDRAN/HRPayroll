c105d7cb3e5d89b8dd6f9ccdb0397c597a904920
SPOOVENDRAN <pooveshanmugam@gmail.com>
2026-01-09 12:21:25 +0530

    FInal push

--- hrpayroll/src/Login/Login.jsx
+++ hrpayroll/src/Login/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   
-
+  window.alert(`${"Employee Sign In - Mail - firstName.lastName@company.com(arun.kumar@company.com) Password - password@123 HR Sign In - HR - hr@company.com password - password@123 Admin Sign In - admin@company.com password - password@123"}`)
   const handleLogin = async () => {
     try {
       const res = await fetch(`${BASE_API_URL}/auth/login`, {
