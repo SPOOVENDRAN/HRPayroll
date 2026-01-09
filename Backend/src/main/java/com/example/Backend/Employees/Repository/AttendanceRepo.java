91334d0a630b9e39ff96234bba96b472d467a76a
Poove <poove93@gmail.com>
2026-01-09 09:16:15 +0530

    env changes

--- Backend/src/main/java/com/example/Backend/Employees/Repository/AttendanceRepo.java
+++ Backend/src/main/java/com/example/Backend/Employees/Repository/AttendanceRepo.java
@@ -1,10 +1,6 @@
 package com.example.Backend.Employees.Repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
-
 import com.example.Backend.Employees.Entity.Attendance;
 
 public interface AttendanceRepo extends JpaRepository<Attendance, Long> {
