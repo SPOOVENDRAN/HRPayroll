62606a5ca27b911f2d4cf027983d148854ef6802
SPOOVENDRAN <pooveshanmugam@gmail.com>
2026-01-09 09:33:41 +0530

    fixed cors

--- Backend/src/main/java/com/example/Backend/security/SecurityConfig.java
+++ Backend/src/main/java/com/example/Backend/security/SecurityConfig.java
@@ -67,7 +67,7 @@ public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Excepti
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
