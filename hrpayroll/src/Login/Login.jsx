69da9e2366b223fa8f5a0ba26834fa38d8bdc2b3
SPOOVENDRAN <pooveshanmugam@gmail.com>
2026-01-22 09:22:38 +0530

    Changed Login Window Prompt

--- hrpayroll/src/Login/Login.jsx
+++ hrpayroll/src/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState , useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Login.css";
 export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
@@ -10,8 +10,8 @@ function Login() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  useEffect(() => {window.alert(`${"Employee Sign In - Mail - firstName.lastName@company.com(arun.kumar@company.com) Password - password@123 HR Sign In - HR - hr@company.com password - password@123 Admin Sign In - admin@company.com password - password@123"}`)},[])
   
-  window.alert(`${"Employee Sign In - Mail - firstName.lastName@company.com(arun.kumar@company.com) Password - password@123 HR Sign In - HR - hr@company.com password - password@123 Admin Sign In - admin@company.com password - password@123"}`)
   const handleLogin = async () => {
     try {
       const res = await fetch(`${BASE_API_URL}/auth/login`, {
