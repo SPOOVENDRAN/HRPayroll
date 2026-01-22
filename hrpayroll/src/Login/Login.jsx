c63f280350b7c302071036d5222495e5ce2c6813
SPOOVENDRAN <pooveshanmugam@gmail.com>
2026-01-22 09:25:16 +0530

    Changed Login Window Prompt

--- hrpayroll/src/Login/Login.jsx
+++ hrpayroll/src/Login/Login.jsx
@@ -10,7 +10,8 @@ function Login() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  useEffect(() => {window.alert(`${"Employee Sign In - Mail - firstName.lastName@company.com(arun.kumar@company.com) Password - password@123 HR Sign In - HR - hr@company.com password - password@123 Admin Sign In - admin@company.com password - password@123"}`)},[])
+  useEffect(() => {window.alert("See Console To get User Credentials")
+console.log(`${"Employee Sign In - Mail - firstName.lastName@company.com(arun.kumar@company.com) Password - password@123 HR Sign In - HR - hr@company.com password - password@123 Admin Sign In - admin@company.com password - password@123"}`)},[])
   
   const handleLogin = async () => {
     try {
